fdc6bcc38d4866e361c57d9e8576523313690d91
Khaled-Aleadini <103153347+khl16@users.noreply.github.com>
2022-07-23 22:23:24 +0300

    CareSoft Interfaces

---
+++ CareSoftInterfaces/src/com/caresoft/clinicapp/Physician.java
@@ -0,0 +1,43 @@
+package com.caresoft.clinicapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Physician extends User implements HIPAACompliantUser  {
+			
+//... imports class definition...
+    
+    public Physician(Integer id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	// Inside class:    
+    private ArrayList<String> patientNotes;
+	
+    // TO DO: Constructor that takes an ID
+    // TO DO: Implement HIPAACompliantUser!
+	
+    public void newPatientNotes(String notes, String patientName, Date date) {
+        String report = String.format( "Datetime Submitted: %s \n", date);
+        report += String.format("Reported By ID: %s\n", this.id);
+        report += String.format("Patient Name: %s\n", patientName);
+        report += String.format("Notes: %s \n", notes);
+        this.patientNotes.add(report);
+    }
+
+	@Override
+	public boolean assignPin(int pin) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean accessAuthorized(Integer confirmedAuthID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+    // TO DO: Setters & Getters
+
+}
